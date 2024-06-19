5cfc638d34099fcbb1addb20742df6b1b89ab890
sixdgt <c4crypt@gmail.com>
2024-06-19 10:23:00 +0545

    CustomerModel method name changed to camel case

--- src/model/CustomerModel.java
+++ src/model/CustomerModel.java
@@ -16,6 +16,126 @@ public class CustomerModel {
     private String credit_status;
     private String applied_at;
     private String verified_at;
+
+    public int getCustomerId() {
+        return customer_id;
+    }
+
+    public void setCustomerId(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getFullName() {
+        return full_name;
+    }
+
+    public void setFullName(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getAnnualIncome() {
+        return annual_income;
+    }
+
+    public void setAnnualIncome(double annual_income) {
+        this.annual_income = annual_income;
+    }
+
+    public String getIncomeSource() {
+        return income_source;
+    }
+
+    public void setIncomeSource(String income_source) {
+        this.income_source = income_source;
+    }
+
+    public double getPropertyValuation() {
+        return property_valuation;
+    }
+
+    public void setPropertyValuation(double property_valuation) {
+        this.property_valuation = property_valuation;
+    }
+
+    public String getPropertyType() {
+        return property_type;
+    }
+
+    public void setPropertyType(String property_type) {
+        this.property_type = property_type;
+    }
+
+    public String getValuationDate() {
+        return valuation_date;
+    }
+
+    public void setValuationDate(String valuation_date) {
+        this.valuation_date = valuation_date;
+    }
+
+    public String getCreditType() {
+        return credit_type;
+    }
+
+    public void setCreditType(String credit_type) {
+        this.credit_type = credit_type;
+    }
+
+    public boolean isIsEligible() {
+        return is_eligible;
+    }
+
+    public void setIsEligible(boolean is_eligible) {
+        this.is_eligible = is_eligible;
+    }
+
+    public double getCreditAmount() {
+        return credit_amount;
+    }
+
+    public void setCreditAmount(double credit_amount) {
+        this.credit_amount = credit_amount;
+    }
+
+    public String getCreditStatus() {
+        return credit_status;
+    }
+
+    public void setCreditStatus(String credit_status) {
+        this.credit_status = credit_status;
+    }
+
+    public String getAppliedAt() {
+        return applied_at;
+    }
+
+    public void setAppliedAt(String applied_at) {
+        this.applied_at = applied_at;
+    }
+
+    public String getVerifiedAt() {
+        return verified_at;
+    }
+
+    public void setVerifiedAt(String verified_at) {
+        this.verified_at = verified_at;
+    }
     
     
 }
