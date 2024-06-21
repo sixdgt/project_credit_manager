03bb9407a684610c70d7b935fd11db31bffe73e2
sixdgt <c4crypt@gmail.com>
2024-06-21 10:37:05 +0545

    AdminLogin Form implemented & AdminDAOImpl code changed

--- src/dao/impl/AdminDAOImpl.java
+++ src/dao/impl/AdminDAOImpl.java
@@ -1,4 +1,5 @@
 package dao.impl;
+
 import controller.AdminController;
 import dao.AdminDAO;
 import dbconfig.DbConnection;
@@ -6,11 +7,11 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import model.AdminModel;
 
-public class AdminDAOImpl implements AdminDAO{
-    
+public class AdminDAOImpl implements AdminDAO {
+
     DbConnection connect = null;
-    
-    public AdminDAOImpl(){
+
+    public AdminDAOImpl() {
         this.connect = DbConnection.getConnection();
     }
 
@@ -21,20 +22,24 @@ public class AdminDAOImpl implements AdminDAO{
         try {
             String sql = "SELECT * FROM admin WHERE admin_username='" + ac.username + "'";
             ResultSet rs = this.connect.selectQueryBuilder(sql);
-            while(rs.next()){
+            while (rs.next()) {
                 // here `admin_username` is a column name of table admin
                 am.setAdminUsername(rs.getString("admin_username"));
                 am.setAdminId(rs.getInt("admin_id"));
                 am.setAdminPassword(rs.getString("admin_password"));
             }
-            if(am.getAdminUsername().equals(ac.username) && 
-                    am.getAdminPassword().equals(ac.password)){
-                status = true;
+            if (am.getAdminUsername() == null || am.getAdminPassword() == null) {
+                status = false;
+            } else {
+                if (am.getAdminUsername().equals(ac.username)
+                        && am.getAdminPassword().equals(ac.password)) {
+                    status = true;
+                }
             }
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
         return status;
     }
-    
+
 }
